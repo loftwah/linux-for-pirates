6d7be47493e445b7f67530f8519f1af92ba0630d
Dean Lofts <dean@deanlofts.xyz>
2025-03-21 23:32:37 +1100

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,28 +7,20 @@ import rehypePrettyCode from 'rehype-pretty-code';
  * @type {import('rehype-pretty-code').Options}
  */
 const prettyCodeOptions = {
-  theme: {
-    light: 'github-light',
-    dark: 'github-dark'
-  },
+  theme: 'one-dark-pro',
+  keepBackground: true,
   onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
     if (node.children.length === 0) {
       node.children = [{ type: 'text', value: ' ' }];
     }
   },
   onVisitHighlightedLine(node) {
-    // Add a class to highlighted lines
     node.properties.className = node.properties.className || [];
     node.properties.className.push('highlighted');
   },
   onVisitHighlightedChars(node) {
-    // Add a class to highlighted words/chars
     node.properties.className = ['word'];
   },
-  // Keep backgrounds and code blocks intact
-  keepBackground: true,
   grid: true
 };
 
